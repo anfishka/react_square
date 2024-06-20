cc563542ed1064f0872d5ea4acbd0ca7d26121ad
Hanna Borodina <venera131916@gmail.com>
2024-06-20 10:37:49 +0300

    add files

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import StyledButton from './StyledButton';
+
+function App() {
+  return (
+    <>
+      <StyledButton/>
+    </>
+  
+   
+  );
+}
+
+export default App;
