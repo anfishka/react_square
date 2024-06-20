38b4c83b29ed196ba14e97b268e0476c0ceab6b6
Hanna Borodina <venera131916@gmail.com>
2024-06-20 10:57:09 +0300

    add component

---
+++ src/components/MyComponents.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+import StyledButton from "../StyledButton";
+
+interface MyComponentProps{ }
+
+const MyComponent: FC<MyComponentProps> = () => (
+    <>
+    <StyledButton />
+    </>
+)
+
+export default MyComponent
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyComponent from './components/MyComponents';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MyComponent/>
   </React.StrictMode>
 );
 
